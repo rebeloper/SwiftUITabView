e10313ce33d520f33c3dd1a5237e9b1ad1970907
Alex Nagy <support@rebeloper.com>
2019-09-18 12:14:44 +0300

    Initial Commit

---
+++ SwiftUITabView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITabView
+//
+//  Created by Agota Matyas on 18/09/2019.
+//  Copyright © 2019 Rebeloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
